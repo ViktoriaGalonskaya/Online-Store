33f884874154c5c378ca0300e2d92da04b540899
ViktoriaGalonskaya <victoriagalonskaya1997@gmail.com>
2019-12-02 15:20:47 +0300

    First commit

---
+++ src/modules/shared/menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+function MenuItem() {
+    return (
+        <li className="menu__item">
+            <a className="menu__link" href="#">new arrivals</a>
+        </li>
+    );
+}
+
+export default MenuItem;
